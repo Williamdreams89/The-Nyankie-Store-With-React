646d400fc92f79ca7776706cc9c4d39458f65da6
= <=>
2022-05-01 17:08:12 +0000

    Home layouting

--- src/Home.js
+++ src/Home.js
@@ -5,12 +5,12 @@ function Home() {
   return (
     <div className='home'>
         <div className='home__container'>
-        <h2>This is the home component</h2>
         <div className='row1'>
-          <h2>This is the home component</h2>
+          <div className='sidebar left__sidebar'>Left</div>
+          <div className='middle__component'>I am the middle man</div>
+          <div className='sidebar right__sidebar'>Right</div>
         </div>
         <div className='row2'>
-          <h2>This is the home component</h2>
         </div>
         </div>
     </div>
