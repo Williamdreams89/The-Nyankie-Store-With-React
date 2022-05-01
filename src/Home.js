1819e629166d874c7b20dc6c69d2a5f320d419d9
= <=>
2022-05-01 16:43:44 +0000

    Home rows created

--- src/Home.js
+++ src/Home.js
@@ -6,6 +6,12 @@ function Home() {
     <div className='home'>
         <div className='home__container'>
         <h2>This is the home component</h2>
+        <div className='row1'>
+          <h2>This is the home component</h2>
+        </div>
+        <div className='row2'>
+          <h2>This is the home component</h2>
+        </div>
         </div>
     </div>
   )
