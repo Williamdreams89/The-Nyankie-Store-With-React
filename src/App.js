f02133bbde1b832750565467e39799af90f02243
= <=>
2022-05-01 08:14:06 +0000

    First Version Created

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Header from './Header';
+
+function App() {
+  return (
+    <div className="App">
+        {/* Header Component */}
+        <Header />
+        {/* Home Component */}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./Header.css"
+
+function Header() {
+  return (
+    <div className="header">
+        <nav className="navbar">
+            <div className="logo__container">Nyankie</div>
+        </nav>
+    </div>
+  )
+}
+
+export default Header
